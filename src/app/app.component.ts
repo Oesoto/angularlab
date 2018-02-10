0ac7f051ba41f5f1c12c4b0dfcc7589df16b99b5
Oscar <oesoto@MacBook-Pro-Touch.local>
2018-02-10 11:39:28 -0500

    Primer release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//ngOnInit is a life cycle hook called by Angular to indicate that Angular is done creating the component.
+import {HttpClient} from '@angular/common/http';
+//HttpClient es traido de HttpClientModule
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  //Toda la lógica la defino dentro de esta clase
+  title = 'Escribe un nombre y mira a que género pertenece y con que probabilidad';
+  baseURL = 'https://api.genderize.io/?name=';
+  respuestaAPI:any; //Puedo recibir cualquier tipo de datos
+  //Variable conectada al input de la vista
+  nombrePersona:String;
+
+  //Inyectar la dependencia. Cada atributo en el constructor es una inyección de dependencia
+  constructor (private http: HttpClient){
+
+  }
+
+  ngOnInit():void{
+    console.log(this.respuestaAPI);
+    // Make the HTTP request:
+    // this.respuestaAPI = this.http.get('https://api.genderize.io/?name=peter');
+
+    //Arrow function
+    // this.http.get('https://api.genderize.io/?name=peter').subscribe((data)=> {
+    //   this.respuestaAPI = data;
+    //   console.log(this.respuestaAPI);
+    // });
+  }
+
+  buscarPersona() {
+    let URLCompleta = this.baseURL + this.nombrePersona;
+    this.http.get(URLCompleta).subscribe((data) => {
+      this.respuestaAPI = data;
+      console.log(this.respuestaAPI);
+    });
+  } 
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
+//HttpClientModule es el modulo de angular para hacer peticiones http
+import {AppComponent} from './app.component';
 
 
 @NgModule({
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    //Cada import en la parte de arriba se debe declarar en en esta zona de imports
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
